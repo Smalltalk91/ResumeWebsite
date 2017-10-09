0cf38845ccbfb6b3306c0b9001e521c888ed97d0
Eric Howe <ehowe@nexient.com>
2017-10-09 15:43:29 -0400

    BLAHHHH

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,9 +4,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class NavigationComponent implements OnInit {
+
+  menu_items = [
+    'Home',
+    'About',
+    'Contact'
+  ];
 
   constructor() { }
 
